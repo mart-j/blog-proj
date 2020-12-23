7bc9683848538ccdb14a2133bf831d2da285cdf2
Old Papa <arturs.buls@gmail.com>
2020-12-23 10:49:01 +0200

    Router and Navi added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import { Home } from './pages/home';
+import { Articles } from './pages/articles';
+import { SignIn } from './pages/sign-in';
 
 const App = () => {
-  return <h1>Hellooo</h1>;
+
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/articles">Articles</Link>
+          </li>
+          <li>
+            <Link to="/sign-in">SignIn</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/sign-in">
+          <SignIn />
+        </Route>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 export default App;
