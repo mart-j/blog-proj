937d9a7086ee9f073b4fd3954c0997452949dacb
Old Papa <arturs.buls@gmail.com>
2020-12-24 18:15:37 +0200

    Added sign out button, already sing in, redirecting to Home page after login

--- src/components/users/users.tsx
+++ src/components/users/users.tsx
@@ -1,16 +1,27 @@
 import React, { FC } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { initialUserStore } from '../../store/userStore/reducer';
 import { RootState } from '../../store';
+import { changeUserAction } from '../../store/userStore/actions';
 
 export const Users: FC = () => {
+  const dispatch = useDispatch();
+
   const currentUser = useSelector((state: RootState) => {
     return state.userStore.user.email;
   });
+  const singOutHandler = () => {
+    dispatch(changeUserAction(initialUserStore.user));
+  };
 
   return (
     <>
       <h3>{currentUser}</h3>
-      <h5>{currentUser !== 'guest' && <button>sign out</button>}</h5>
+      <h5>
+        {currentUser !== 'guest' && (
+          <button onClick={singOutHandler}>Sign out</button>
+        )}
+      </h5>
     </>
   );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,6 @@
 import React, { FC, useState } from 'react';
+import _ from 'lodash';
+import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store';
 import { changeUserAction } from '../store/userStore/actions';
@@ -15,45 +17,52 @@ export const Login: FC = () => {
     return state.userStore.userDb;
   });
 
+  const history = useHistory();
+
   const dispatchHandler = () => {
-    if (JSON.stringify(userDb).includes(JSON.stringify(input))) {
+    if (_.some(userDb, input)) {
       dispatch(changeUserAction(input!));
+      history.push('/');
     }
   };
 
   return (
     <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          dispatchHandler();
-          setInput({ email: '', password: '' });
-        }}
-      >
-        <label htmlFor="fname">e-mail: </label>
-        <input
-          onChange={(e) => {
-            setInput({ ...input!, email: e.target.value });
+      {user.email === 'guest' ? (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatchHandler();
+            setInput({ email: '', password: '' });
           }}
-          type="email"
-          value={input.email}
-          required
-        />
-        <br />
-        <br />
-        <label htmlFor="lname">Password: </label>
-        <input
-          onChange={(e) => {
-            setInput({ ...input!, password: e.target.value });
-          }}
-          type="password"
-          value={input.password}
-          required
-        />
-        <br />
-        <br />
-        <input type="submit" />
-      </form>
+        >
+          <label htmlFor="fname">e-mail: </label>
+          <input
+            onChange={(e) => {
+              setInput({ ...input!, email: e.target.value });
+            }}
+            type="email"
+            value={input.email}
+            required
+          />
+          <br />
+          <br />
+          <label htmlFor="lname">Password: </label>
+          <input
+            onChange={(e) => {
+              setInput({ ...input!, password: e.target.value });
+            }}
+            type="password"
+            value={input.password}
+            required
+          />
+          <br />
+          <br />
+          <input type="submit" />
+        </form>
+      ) : (
+        'You are already signed in!'
+      )}
     </>
   );
 };
