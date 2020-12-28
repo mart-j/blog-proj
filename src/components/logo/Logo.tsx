3e07988c1704b45e2377d74747a9a4c34523e97e
mart-j <m.jukss@gmail.com>
2020-12-28 10:41:31 +0200

    aligned header

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,11 +3,13 @@ import styles from './Logo.module.scss';
 
 export const Logo: FC = () => {
   return (
-    <div className={styles.logoWrapper}>
-      <div className={styles.logoSquare}>
-        <div className={styles.logoText}>devBlog</div>
+    <div className={styles.container}>
+      <div className={styles.logoWrapper}>
+        <div className={styles.logoSquare}>
+          <div className={styles.logoText}>devBlog</div>
+        </div>
+        <div className={styles.logoSquare}></div>
       </div>
-      <div className={styles.logoSquare}></div>
     </div>
   );
 };
